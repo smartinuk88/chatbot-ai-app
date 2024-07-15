ca141804e015ca47767c39eb0f815d42616ad142
Scott Martin <scott.martin88@live.com>
2024-07-15 08:04:40 +0200

    Create custom Apollo provider wrapper

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
+import ApolloProviderWrapper from "@/components/ApolloProvider";
 
 export const metadata: Metadata = {
   title: "Chatbot AI",
@@ -13,10 +14,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body className="flex min-h-screen">{children}</body>
-      </html>
-    </ClerkProvider>
+    <ApolloProviderWrapper>
+      <ClerkProvider>
+        <html lang="en">
+          <body className="flex min-h-screen">{children}</body>
+        </html>
+      </ClerkProvider>
+    </ApolloProviderWrapper>
   );
 }
---
+++ components/ApolloProvider.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import client from "@/graphql/apolloClient";
+import { ApolloProvider } from "@apollo/client";
+
+const ApolloProviderWrapper = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return <ApolloProvider client={client}>{children}</ApolloProvider>;
+};
+
+export default ApolloProviderWrapper;
