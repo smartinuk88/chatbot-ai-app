72c4c3198af66882a3f21d076d35153dbac56fb9
Scott Martin <scott.martin88@live.com>
2024-09-18 16:24:32 +0200

    Build ViewChatbots page

--- app/(admin)/view-chatbots/page.tsx
+++ app/(admin)/view-chatbots/page.tsx
@@ -18,7 +18,9 @@ async function ViewChatbots() {
 
   console.log("USERID:", userId);
 
-  const result = await serverClient.query<
+  const {
+    data: { chatbotsByUser },
+  } = await serverClient.query<
     GetChatbotsByUserData,
     GetChatbotsByUserDataVariables
   >({
@@ -28,19 +30,76 @@ async function ViewChatbots() {
     },
   });
 
-  console.log("Query result:", result);
-
   // Sort by created_at so that the most recent chatbot created is displayed first
-  // const sortedChatbotsByUser: Chatbot[] = [...chatbotsByUser].sort(
-  //   (a, b) =>
-  //     new Date(b.created_at).getTime() - new Date(a.created_at).getTime()
-  // );
+  const sortedChatbotsByUser: Chatbot[] = [...chatbotsByUser].sort(
+    (a, b) =>
+      new Date(b.created_at).getTime() - new Date(a.created_at).getTime()
+  );
 
   return (
-    <div>
+    <div className="flex-1 pb-20 p-10">
       <h1 className="text-xl lg:text-3xl font-semibold mb-5">
         Active Chatbots
       </h1>
+
+      {sortedChatbotsByUser.length === 0 && (
+        <div>
+          <p>
+            You have not created any chatbots yet. Click the button below to
+            create one.
+          </p>
+          <Link href="/create-chatbot">
+            <Button className="bg-[#64B5F5] text-white p-3 rounded-md mt-5">
+              Create Chatbot
+            </Button>
+          </Link>
+        </div>
+      )}
+
+      <ul className="flex flex-col space-y-5">
+        {sortedChatbotsByUser.map((chatbot) => (
+          <Link key={chatbot.id} href={`/edit-chatbot/${chatbot.id}`}>
+            <li
+              key={chatbot.id}
+              className="relative p-10 border rounded-md max-w-3xl bg-white"
+            >
+              <div className="flex justify-between items-center">
+                <div className="flex items-center space-x-4">
+                  <Avatar seed={chatbot.name} />
+                  <h2 className="text-xl font-bold">{chatbot.name}</h2>
+                </div>
+
+                <p className="absolute top-5 right-5 text-xs text-gray-400">
+                  Created: {new Date(chatbot.created_at).toLocaleString()}
+                </p>
+              </div>
+
+              <hr className="mt-2" />
+
+              <div className="grid grid-cols-2 gap-10 md:gap-5 p-5">
+                <h3 className="italic">Characteristics:</h3>
+
+                <ul className="text-xs">
+                  {!chatbot.chatbot_characteristics.length && (
+                    <p>No characteristics added yet.</p>
+                  )}
+                  {chatbot.chatbot_characteristics.map((characteristic) => (
+                    <li
+                      className="list-disc break-words"
+                      key={characteristic.id}
+                    >
+                      {characteristic.content}
+                    </li>
+                  ))}
+                </ul>
+
+                <h3 className="italic">No of Sessions:</h3>
+                <p>{chatbot.chat_sessions.length}</p>
+              </div>
+            </li>
+          </Link>
+        ))}
+      </ul>
     </div>
   );
 }
