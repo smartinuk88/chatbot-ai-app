ea448d6836ab496ef0871225f6a2a26fbc460f90
Scott Martin <scott.martin88@live.com>
2024-09-19 07:27:06 +0200

    Build review session page

--- types/types.ts
+++ types/types.ts
@@ -56,10 +56,28 @@ export interface GetChatbotsByUserDataVariables {
   clerk_user_id: string;
 }
 
+export interface GetUserChatbotsResponse {
+  chatbotsByUser: Chatbot[];
+}
 export interface GetUserChatbotsVariables {
   userId: string;
 }
 
-export interface GetUserChatbotsResponse {
-  chatbotsByUser: Chatbot[];
+export interface GetChatSessionMessagesResponse {
+  chat_sessions: {
+    id: number;
+    created_at: string;
+    messages: Message[];
+    chatbots: {
+      name: string;
+    };
+    guests: {
+      name: string;
+      email: string;
+    };
+  };
+}
+
+export interface GetChatSessionMessagesVariables {
+  id: number;
 }
