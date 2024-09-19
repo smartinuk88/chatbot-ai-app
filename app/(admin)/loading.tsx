ae4aaa5ebb41e4db8a3ec6432166128f686a7c4b
Scott Martin <scott.martin88@live.com>
2024-09-19 10:20:56 +0200

    Build loading page

---
+++ app/(admin)/loading.tsx
@@ -0,0 +1,11 @@
+import Avatar from "@/components/Avatar";
+
+function loading() {
+  return (
+    <div className="mx-auto animate-spin p-10">
+      <Avatar seed="assistly" />
+    </div>
+  );
+}
+
+export default loading;
